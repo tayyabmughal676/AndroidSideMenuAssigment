9f341dfe7294f716d108b32645a521a66778548d
Tayyabmughal676 <Tayyabmughal676@gmail.com>
2020-12-19 18:55:42 +0500

    Initial commit

---
+++ app/src/main/java/com/example/androidassigment/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.androidassigment.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is MAD slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
